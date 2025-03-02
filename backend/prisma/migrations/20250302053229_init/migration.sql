5f95bb3484273cc64ead4f05d2dadb70f13c782b
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2025-03-02 16:42:12 +1100

    Added the database tables

---
+++ backend/prisma/migrations/20250302053229_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resume" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "personalInfo" JSONB NOT NULL,
+    "summary" TEXT,
+    "skills" JSONB NOT NULL,
+    "experience" JSONB NOT NULL,
+    "education" JSONB NOT NULL,
+    "projects" JSONB,
+    "certifications" JSONB,
+    "involvement" JSONB,
+    "publications" JSONB,
+    "languages" JSONB,
+    "references" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Resume_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Resume" ADD CONSTRAINT "Resume_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
