35ddbad44f1faa301c290bd58da8caa15259023c
souravsharma <msg2souravsharma@gmail.com>
2025-03-02 18:29:06 +1100

    Front-end login page UI only  and splashscreen loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, Suspense, lazy } from 'react';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
 import './App.css';
+import LoginPage from './app/login/page';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function lazyWithDelay(factory, delay = 2500) {
+  return lazy(() =>
+    Promise.all([
+      factory(),
+      new Promise(resolve => setTimeout(resolve, delay))
+    ]).then(([moduleExports]) => moduleExports)
+  );
+}
+
+const Login = lazyWithDelay(() => import('./app/login/page'), 2500);
+
+
+const splash = (
+  <DotLottieReact
+    src="https://lottie.host/306a447b-87d4-4729-a9d9-91f642bc399c/B4V1Qj3kKO.lottie"
+    loop
+    autoplay
+  />
+)
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -34,20 +56,32 @@ const App = () => {
   }, []);
 
   return (
-    <div className="app-container">
-      <button
-        onClick={handleGeneratePdf}
-        disabled={isLoading}
-        className="generate-button"
-      >
-        {isLoading ? 'Generating...' : 'Generate  PDF'}
-      </button>
-      {errorMessage && (
-        <p className="error-message" style={{ color: 'red' }}>
-          {errorMessage}
-        </p>
-      )}
-    </div>
+    <BrowserRouter>
+      <Suspense fallback= {splash}>
+      <div className="fade-in">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Suspense>
+    
+    
+    </BrowserRouter>
+    // <div className="app-container">
+    //   <Button
+    //     onClick={handleGeneratePdf}
+    //     disabled={isLoading}
+    //     className="generate-button"
+    //   >
+    //     {isLoading ? 'Generating...' : 'Generate  PDF'}
+    //   </Button>
+    //   {errorMessage && (
+    //     <p className="error-message" style={{ color: 'red' }}>
+    //       {errorMessage}
+    //     </p>
+    //   )}
+    // </div>
   );
 };
 
