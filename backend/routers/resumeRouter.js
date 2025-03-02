28a6c8291812136869d81565d740832235c777b0
Shubham Sharma <87312848+shubhams-git@users.noreply.github.com>
2025-03-02 20:38:45 +1100

    Experimenting with the backend architecture

--- backend/routers/resumeRouter.js
+++ backend/routers/resumeRouter.js
@@ -2,7 +2,10 @@ import express from 'express'
 
 const resumeRouter = express.Router()
 
-
-
+resumeRouter.post("/create", authenticateUser, createResume);
+resumeRouter.get("/fetch/:id", authenticateUser, getResume);
+resumeRouter.put("/update/:id", authenticateUser, updateResume);
+resumeRouter.delete("/delete/:id", authenticateUser, deleteResume);
+resumeRouter.post("/generate-pdf/:resumeId", authenticateUser, generateResumePDF);
 
 export default resumeRouter
